838f9723c308095768d1588ed42d0a9bbede4c08
ipincamp <support@nur-arifin.my.id>
2024-11-27 21:02:36 +0700

    implement standarization for json response

---
+++ app/Helpers/ApiResponseHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+class ApiResponseHelper
+{
+    /**
+     * Generate success response.
+     *
+     * @param string $message
+     * @param array|object $data
+     * @return JsonResponse
+     */
+    public static function success(string $message, $data = []): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+            'errors' => null,
+        ], 200);
+    }
+
+    /**
+     * Generate error response.
+     *
+     * @param string $message
+     * @param array|object|null $errors
+     * @param int $statusCode
+     * @return JsonResponse
+     */
+    public static function error(string $message, $errors = null, int $statusCode = 400): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => [],
+            'errors' => $errors,
+        ], $statusCode);
+    }
+}
