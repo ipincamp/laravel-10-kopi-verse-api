c0a24327487da6efdb56a4c2f5f1cf56be1b9d30
ipincamp <support@nur-arifin.my.id>
2024-12-13 03:09:27 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,10 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            PermissionSeeder::class,
-            UserSeeder::class,
-            ProductSeeder::class,
-        ]);
+        // permission & role
+        collect(['customer', 'cashier', 'admin'])->each(fn($role) => Role::create(['name' => $role]));
+
+        // user
+        $roles = ['admin', 'cashier', 'customer'];
+        foreach ($roles as $role) {
+            $user = User::create([
+                'name' => config("custom.$role.name"),
+                'email' => config("custom.$role.email"),
+                'password' => bcrypt(config("custom.$role.password")),
+            ]);
+            // assign role
+            $user->assignRole($role);
+            // create cart
+            $user->cart()->create();
+        }
+
+        // product
+        Product::factory(10)->create();
     }
 }
