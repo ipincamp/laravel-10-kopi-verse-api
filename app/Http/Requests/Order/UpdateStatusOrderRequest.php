7d062ff5b8564392d1c2273ad202efcffbc901a4
ipincamp <support@nur-arifin.my.id>
2024-12-14 12:15:17 +0700

    create order dto when updating status order

--- app/Http/Requests/Order/UpdateStatusOrderRequest.php
+++ app/Http/Requests/Order/UpdateStatusOrderRequest.php
@@ -3,16 +3,15 @@
 namespace App\Http\Requests\Order;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
-class ValidateOrderCodeRequest extends FormRequest
+class UpdateStatusOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return Auth::check();
+        return true;
     }
 
     /**
@@ -23,7 +22,8 @@ class ValidateOrderCodeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'order_code' => ['required', 'string', 'exists:orders,code'],
+            'notes' => ['nullable', 'string'],
+            'status' => ['required', 'string', 'in:prep,ready,done,cancel'],
         ];
     }
 }
